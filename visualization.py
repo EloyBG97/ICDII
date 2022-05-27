5a9b528414e3d39af464a032db7ba91cdfd7112f
eloybg97 <eloybg97@uma.es>
2022-05-27 21:49:48 +0200

    Added data cleaning

---
+++ visualization.py
@@ -0,0 +1,123 @@
+from pymongo import MongoClient
+import pandas as pd
+from sklearn.impute import KNNImputer
+from datetime import datetime
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+def dist(X, Y, metric, **kwds):
+    total = 0
+
+    if(X["provincia"] != Y["provincia"]):
+        total += 1
+
+    if(X["com_autonoma"] != Y["com_autonoma"]):
+        total += 1
+
+    total += abs(X["year"] - Y["year"])
+    total += abs(X["month"] - Y["month"])
+
+    return total
+
+def scale(unit, level):
+
+    if(unit == "µg/m3"):
+        level *= 10e-6
+
+    elif(unit == "mg/m3"):
+        level *= 10e-3
+
+    elif(unit == "ng/m3"):
+        level *= 10e-9
+
+    return level
+
+
+def main():
+    client = MongoClient('mongodb+srv://admin:ICFy36tkvEQcFHY8@cluster0.itzr6.mongodb.net/test')
+    result = client['ICDII']['AirQuality'].aggregate([
+        {
+            '$unwind': {
+                'path': '$data'
+            }
+        }, {
+            '$addFields': {
+                'data.avg_level': {
+                    '$avg': [
+                        '$data.values'
+                    ]
+                }
+            }
+        }, {
+            '$group': {
+                '_id': {
+                    'pollutant': '$data.pollutant', 
+                    'unit': '$data.unit', 
+                    'date': '$date', 
+                    'location': {
+                        'com_autonoma': '$location.com_autonoma', 
+                        'provincia': '$location.provincia'
+                    }
+                }, 
+                'avg_level': {
+                    '$avg': '$data.avg_level'
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 0, 
+                'pollutant': '$_id.pollutant', 
+                'unit': '$_id.unit', 
+                'year': '$_id.date.year', 
+                'month': '$_id.date.month', 
+                'provincia': '$_id.location.provincia', 
+                'com_autonoma': '$_id.location.com_autonoma', 
+                'avg_level': 1
+            }
+        }
+    ])
+
+
+    #Step 1: Load Data
+    list_cur = list(result)
+    df = pd.DataFrame(list_cur)
+
+    # Solo avg_level tiene NaN values
+    #for col in df:
+    #   print(len(df[df[col].isna()]))
+
+    #Step 1: Imputate 'avg_level' values using KNN method 
+    df["pollutant"] = df["pollutant"].astype('category')
+    df["com_autonoma"] = df["com_autonoma"].astype('category')
+    df["provincia"] = df["provincia"].astype('category')
+
+    df["pollutant_cat"] = df["pollutant"].cat.codes
+    df["com_autonoma_cat"] = df["com_autonoma"].cat.codes
+    df["provincia_cat"] = df["provincia"].cat.codes
+
+    imputer = KNNImputer(n_neighbors = 4)
+    df["avg_level"] = imputer.fit_transform(df[["avg_level", "pollutant_cat", "com_autonoma_cat", "provincia_cat", "year", "month"]])[:,0]
+
+    del df["pollutant_cat"]
+    del df["com_autonoma_cat"]
+    del df["provincia_cat"]
+    
+    #Step 2: Normalize scales (unit)
+    df["avg_level"] = df.apply(lambda row: scale(row["unit"], row["avg_level"]), axis = 1)
+    del df["unit"]
+
+
+    #Step 3: Datetime
+    df['Date'] = df.apply(lambda row: datetime(row["year"], row["month"], 1), axis = 1)
+    del df["year"]
+    del df["month"]
+
+    
+    print(df.head())
+
+    #Visualize...
+
+
+if(__name__ == '__main__'):
+    main()
